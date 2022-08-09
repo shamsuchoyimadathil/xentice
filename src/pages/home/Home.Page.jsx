74752a67c03d0c0f3c6e51c34ef1abde611d18ed
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2022-08-09 11:30:14 +0530

    navbar and home form Banner

--- src/pages/home/Home.Page.jsx
+++ src/pages/home/Home.Page.jsx
@@ -1,13 +1,19 @@
 import { Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
+import FormBanner from '../../components/partials/home/FormBanner'
 import Navbar from '../../components/partials/navs/Navbar'
 import '../../scss/home/_home.scss'
+import {
+  drawerDatas,
+  drawerDividerDatas1,
+} from "../../assets/datas/home/drawerDatas";
 
 function HomePage() {
   return (
     <Box>
-        <Navbar/>
+        <Navbar drawerDatas={drawerDatas}  drawerDividerDatas1={ drawerDividerDatas1}/>
+        <FormBanner/>
     </Box>
   )
 }
