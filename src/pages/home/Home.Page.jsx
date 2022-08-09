e3d716bc2ccffa26f4c5e8be2ebffa24a9fcffec
shamsuchoyimadathil <shamsudheenchoyimadathil@gmail.com>
2022-08-09 08:05:58 +0530

    initial commit/project setup

---
+++ src/pages/home/Home.Page.jsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import { Box } from '@mui/system'
+import React from 'react'
+import Navbar from '../../components/partials/navs/Navbar'
+import '../../scss/home/_home.scss'
+
+function HomePage() {
+  return (
+    <Box>
+        <Navbar/>
+    </Box>
+  )
+}
+
+export default HomePage
